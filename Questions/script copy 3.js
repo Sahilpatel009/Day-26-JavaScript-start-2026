34511c68de801c5c5d99c342c473cac11de44215
Sahil_Patel <sahilbpatel009@gmail.com>
2026-01-20 21:05:27 +0530

    Solve some Questions

--- Questions/script copy 3.js
+++
@@ -1,6 +0,0 @@
-// Q.1 Print Number From 1 to 10 using for loop .
-
-for (let i = 1; i < 11; i++) {
-    console.log(i);
-
-}
--- Questions/script.js
+++ Questions/script.js
@@ -1,6 +1,48 @@
 // Q.1 Print Number From 1 to 10 using for loop .
 
-for (let i = 1; i < 11; i++) {
-    console.log(i);
+// for (let i = 1; i < 11; i++) {
+//     console.log(i);
 
-}
+// }
+
+
+// Q.2 print even number from 1 to 20
+// let i=1;
+// for(i=1;i<=20;i++){
+//     if(i%2===0){
+//         console.log(i);
+
+//     }
+// }
+
+// Q.3 print number from 10 to 1.
+
+// for(let i=10;i>0;i--){
+//     console.log(i);
+
+// }
+
+// Q.4 print the word "Yes" 5 times.
+
+// for (let i = 1; i < 6; i++) {
+//     console.log("Yes");
+
+// }
+
+//Q.5 print weather number from 1 to 10 are even or odd .
+
+// let i = 1;
+// for (i = 1; i < 11; i++) {
+//     if (i % 2 === 0) {
+//         console.log(`${i} - Even`);
+
+//     }
+//     else
+//      console.log(`${i} - Odd`);
+// }
+
+// Q.6 Ask user for a number and say if it is  positive or negative .
+
+let num = prompt("Number Bolo");
+if (num > 0) console.log(`${num} - Positive`);
+else   console.log(`${num} - Negative`);
