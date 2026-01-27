93f01643dfac20fd9f97779bac7b71b17199b4fa
Sahil_Patel <sahilbpatel009@gmail.com>
2026-01-27 13:37:16 +0530

    Solve some Questions

--- Questions/script.js
+++ Questions/script.js
@@ -11,23 +11,23 @@
 // for(i=1;i<=20;i++){
 //     if(i%2===0){
 //         console.log(i);
-
 //     }
 // }
+
 
 // Q.3 print number from 10 to 1.
 
 // for(let i=10;i>0;i--){
 //     console.log(i);
-
 // }
+
 
 // Q.4 print the word "Yes" 5 times.
 
 // for (let i = 1; i < 6; i++) {
 //     console.log("Yes");
-
 // }
+
 
 //Q.5 print weather number from 1 to 10 are even or odd .
 
@@ -47,7 +47,7 @@
 // if (num >= 0) console.log(`${num} - Positive`);
 // else console.log(`${num} - Negative`);
 
-// Ask user age and check if eligible to vote
+// Q.7 Ask user age and check if eligible to vote
 // if age = 18 -> Eligible ,else = not eligible.
 
 // let age = prompt("Apna Age Dalo");
@@ -63,14 +63,14 @@
 //     console.error("You Cancelled it");
 // }
 
-// Print multiplication table of 5 
+//Q.8 Print multiplication table of 5 
 
 // for (let i = 1; i < 11; i++) {
 //     console.log(`5 X ${i} = ${5 * i}`);
 
 // }
 
-// count how many numbers  between 1 and 15 are greater than 8
+// Q.9 count how many numbers  between 1 and 15 are greater than 8
 
 // let count = 0;
 // for (let i = 1; i < 16; i++) {
@@ -80,7 +80,7 @@
 // }
 // console.log(count);
 
-// Ask user for passward and print access status .
+// Q.10 Ask user for passward and print access status .
 
 // let passward = "sahilbhai";
 // let pass = prompt("Phle Passward dal laude");
@@ -91,3 +91,12 @@
 //     console.log("Tu to gya Beta Passward Galat hai 😁");
 
 // }
+
+// Q.11 Level 2 question ----->>>
+
+// Allow only 3 attemps  to enter correct password 
+// if user get it right early stop.
+// if not --> Account locked.
+
+let pass = prompt("Password de do");
+while (pass !== stop) { }
