34fd9815f24d288b701d1cfe6a97fec9f798cb19
Sahil_Patel <sahilbpatel009@gmail.com>
2026-02-25 20:45:52 +0530

    function sum whit array

--- function/script.js
+++ function/script.js
@@ -1,3 +1,9 @@
-const obj = { name: 'Riya' };
-obj.age = 21;
-console.log(obj);
+function sumArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+console.log(sumArray([15, 6, 10, 2]));
