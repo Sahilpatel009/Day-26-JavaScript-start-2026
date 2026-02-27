c2ef740800a95f5fa81eeda5138e19e4c2d57186
Sahil_Patel <sahilbpatel009@gmail.com>
2026-02-27 19:43:19 +0530

    logical  operator

--- function/script.js
+++ function/script.js
@@ -1,5 +1,3 @@
-const sum = 5 + 3; // Addition
-const diff = 10 - 2; // Subtraction
-const p = 4 * 2; // Multiplication
-const q = 8 / 2; // Division
-console.log(sum, diff, p, q);
+const a = true, b = false;
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
