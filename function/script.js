8496141865c35756221e7d7c84b07a1e56160cc5
Sahil_Patel <sahilbpatel009@gmail.com>
2026-02-27 19:41:47 +0530

    multiple operator

--- function/script.js
+++ function/script.js
@@ -1,7 +1,5 @@
-function mergeArrays(arr1, arr2) {
-    // this method merges all the elements 
-    // of arr2 at the end of arr1.
-    return arr1.concat(arr2);
-}
-
-console.log(mergeArrays([5, 6], [7, 8]));
+const sum = 5 + 3; // Addition
+const diff = 10 - 2; // Subtraction
+const p = 4 * 2; // Multiplication
+const q = 8 / 2; // Division
+console.log(sum, diff, p, q);
