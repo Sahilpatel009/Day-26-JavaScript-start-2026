b778cb7ad511c8321f831e275fc4302c51461680
Sahil_Patel <sahilbpatel009@gmail.com>
2026-02-28 20:40:55 +0530

    function

--- function/script.js
+++ function/script.js
@@ -1,3 +1,17 @@
-(function() {
-    console.log("This is a self-invoking function!");
-})();
+class Dog {
+    constructor(name, breed) {
+      
+        // instance property  
+        this.name = name;   
+        this.breed = breed; 
+    }
+
+    // defining method
+    bark() {
+        console.log(`${this.name} says woof!`);
+    }
+}
+
+// creating an object (instance)
+const myDog = new Dog("Rayne", "Husky");
+myDog.bark();
