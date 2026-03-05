c3ae6333a28c874fa4e0078a0fd1af2d1dd037fd
Sahil_Patel <sahilbpatel009@gmail.com>
2026-03-05 16:22:05 +0530

    Loop print number from 1 to 10

--- function/script.js
+++ function/script.js
@@ -1,24 +1,5 @@
-let a = prompt("Enter your number to show your grade");
-let grade;
-let result;
-if (a >= 90 && a < 100) {
-  grade = "A";
-  result = "Excellent";
+// print number from 1 to 10
+for(let i=1; i<=10;i++){
+  console.log(i);
+  
 }
-else if (a >= 70 && a <= 89) {
-  grade = "B";
-  result = "Good";
-}
-else if (a >= 60 && a <= 69) {
-  grade = "C";
-  result = "Average";
-}
-else if (a >= 50 && a <= 59) {
-  grade = "D";
-  result = "Below Average";
-}
-else if (a >= 0 && a <= 49) {
-  grade = "F";
-  result = "Fail";
-}
-console.log(`According to your score, your grade is: ${grade} and your result is: ${result}`);
