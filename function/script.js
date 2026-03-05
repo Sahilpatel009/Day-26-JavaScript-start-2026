be273f16fb964bd9ec13124ccdbeef4f7c4cd3b6
Sahil_Patel <sahilbpatel009@gmail.com>
2026-03-05 11:06:31 +0530

    even or odd

--- function/script.js
+++ function/script.js
@@ -1,10 +1,8 @@
-const product = {
-  name: "pen",
-  color: "black",
-  rating: 4,
-  dod: true,
-  price: 270,
-  offer: 5,
-};
-console.log(product);
+let a = prompt("enter number to chech even or odd");
+if (a % 2 == 0) {
+  console.log(`${a} is Even Numbe`);
+}
+else {
+  console.log(`${a} is Odd Number`);
 
+}
