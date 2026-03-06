fdf059317382f2ca663242e270f134203000dba9
Sahil_Patel <sahilbpatel009@gmail.com>
2026-03-06 20:00:42 +0530

    print all even and  odd number from 0 to 100 and also count total no of even and odd numbers are present from 0 to 100

--- loops/scrpt.js
+++ loops/scrpt.js
@@ -1,5 +1,18 @@
-// print all number from 0 to 100
-for (let i = 0; i <= 100; i++) {
-    console.log(i);
+// print all even and  odd number from 0 to 100 
+// and also count total no of even and odd numbers are present from 0 to 100
+let even = 0;
+let odd = 0;
+for (i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} is Even number`);
+        even++;
+    }
+    else {
+        console.log(`${i} is a Odd number`);
+        odd++;
 
+    }
 }
+console.log(`Total Even number are = ${even}`)
+console.log(`Total Odd number are = ${odd}`)
+
