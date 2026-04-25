9bbef656225153d21b200a5cd6932246bddf74d3
Sahil Patel <sahilpatel009@gmail.com>
2026-04-25 11:01:25 +0530

    update from new laptop i created data typr string and function folder and do some code

---
+++ dataType/script.js
@@ -0,0 +1,4 @@
+let a="sahil";
+let b=10;
+let c=a+b;
+console.log(c);
--- operator/script.js
+++ operator/script.js
@@ -1,3 +1,5 @@
+//ternary operator
+
 let a = 5;
 let result = a > 19 ? "you are eligble" : "no you are not eligble";
 console.log(re);
