3e6ae310f9d8904491dc185bd14a50130debb26b
Sahil Patel <sahilpatel009@gmail.com>
2026-04-26 13:54:30 +0530

    array in js today i practice on array method pop push shift unshift and length of array

--- array/script.js
+++ array/script.js
@@ -1,3 +1,20 @@
-let a = 5;
-let result = a > 19 ? "you are eligble" : "no you are not eligble";
-console.log(re);
+// let a = 5;
+// let result = a > 19 ? "you are eligble" : "no you are not eligble";
+// console.log(re);
+
+// Loop in Array
+// let arr = ["sahil", "mohan", "ram", "karan", "shivam"]
+// for (let idx = 0; idx < arr.length; idx++) 
+// {
+//     console.log(arr[idx]);
+// }
+
+// method pop 
+
+let car = ["batman", "superman", "spiderman", "ironman"]
+// car.pop();
+// car.push("sahil");
+// car.shift();
+// car.unshift("Thor the papa");
+car.length;
+console.log(car);
