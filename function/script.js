5be709c51eac7a729c883b2f230b42ed19bd0b95
Sahil Patel <sahilpatel009@gmail.com>
2026-04-26 17:35:21 +0530

    function tested

--- function/script.js
+++ function/script.js
@@ -1,5 +1,12 @@
-// print number from 1 to 10
-for(let i=1; i<=10;i++){
-  console.log(i);
-  
+function myfunction() {
+
+  console.log("function 1 tested");
+  console.log("function 2 tested");
+
+  // print number from 1 to 10
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+
 }
+myfunction();
