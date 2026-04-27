b34810b3f8770bdcb1413865eba8d019fdde0546
Sahil Patel <sahilpatel009@gmail.com>
2026-04-27 11:13:58 +0530

    vowel count

--- function/script.js
+++ function/script.js
@@ -1,12 +1,31 @@
-function myfunction() {
+// function myfunction() {
 
-  console.log("function 1 tested");
-  console.log("function 2 tested");
+//   console.log("function 1 tested");
+//   console.log("function 2 tested");
 
-  // print number from 1 to 10
-  for (let i = 1; i <= 10; i++) {
-    console.log(i);
+//   // print number from 1 to 10
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+
+// }
+// myfunction();
+
+// vowel count from text charatcer
+
+function countVowel(str) {
+  let count = 0;
+  for (const char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
   }
-
+  return count;
 }
-myfunction();
+countVowel("sahil");
